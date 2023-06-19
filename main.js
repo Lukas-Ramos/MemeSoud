38bcb3220eb152ff51d7228d502ca5bfc6dec10a
Lucas Ramos Magalhães <113211755+Lukas-Ramos@users.noreply.github.com>
2023-06-19 13:49:21 -0300

    Adicionado Projeto

---
+++ main.js
@@ -0,0 +1,52 @@
+function tocaSom (seletorAudio){
+   const elemento  = document.querySelector (seletorAudio);
+
+   if (elemento != null && elemento.localName ==='audio'){
+            elemento.play();
+        }
+     else {
+         // alert('Elemento não encontrado');
+         console.log('Elemento não encontrado ou seletor inválido');
+     }
+   }
+
+
+
+const listaDeTeclas = document.querySelectorAll ('.tecla');
+
+let contador = 0;
+
+
+for(let contador = 0; contador < listaDeTeclas.length; contador++){
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`;
+   // console.log(idAudio);
+    
+    tecla.onclick = function() {
+        tocaSom(idAudio);
+        
+    }
+
+    tecla.onkeydown = function(evento) {
+
+        if (evento.code === 'Space' || evento.code === 'Enter') {
+            tecla.classList.add('ativa');
+        }
+       
+
+
+    }
+    tecla.onkeyup = function() {
+        tecla.classList.remove('ativa');
+    }
+    //console.log(contador);
+    
+}
+
+
+
+
+
+
